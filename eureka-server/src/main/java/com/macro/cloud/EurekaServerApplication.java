687c6ae3103ce6849ab9a267d878dfbcc85abf5e
“e <1922375530@qq.com>
2021-02-04 17:51:18 +0800

    1

---
+++ eureka-server/src/main/java/com/macro/cloud/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.macro.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka 注册中心
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
